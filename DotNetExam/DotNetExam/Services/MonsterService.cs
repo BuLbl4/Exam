6a6df245c2bb3fffe8bfb9180898889e1b20cd08
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2024-01-09 15:00:34 +0300

    done

--- DotNetExam/DotNetExam/Services/MonsterService.cs
+++ DotNetExam/DotNetExam/Services/MonsterService.cs
@@ -13,9 +13,8 @@ public class MonsterService : IMonsterService
    
     public Monster GetRandomMonster()
     {
-        IEnumerable<Monster> monsters = _context.Monsters!;
         var rnd = new Random();
-        var id = rnd.Next(1,monsters.Count() + 1);
-        return monsters.First(m => m.Id == id);
+        var randomIndex = rnd.Next(0, _context.Monsters.Count() + 1);
+        return _context.Monsters.OrderBy(m => m.Id).Skip(randomIndex).FirstOrDefault();
     }
 }
